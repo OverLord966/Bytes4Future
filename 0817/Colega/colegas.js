1cb0f0feddf6a05d818ec53da24f5295b80659a8
OverLord966 <daabea2001@gmail.com>
2022-08-25 21:50:53 +0100

    test

--- 0817/Colega/colegas.js
+++ 0817/Colega/colegas.js
@@ -4,6 +4,8 @@ function ola(Diogo) {
 function olaF(Francisco) {
   console.log("Olá");
 }
-function msi(home){
-  finalmente
+function msi(home, n){
+  return console.log(home.filter((a) => a === n))
 }
+
+console.log("ola mans", "a")
